f48634f29ec32b0b8abd4da2f87e6e037fcb22c2
Paul Hall <paul_hall@brown.edu>
2020-09-30 17:38:39 -0400

    Added various user ID fields.

--- PremiumReport.py
+++ PremiumReport.py
@@ -1,25 +1,135 @@
-import group
+import grp
 import pwd
 import subprocess
 import pandas as pd
 
 # DEFINE FUNCTIONS
-def get_members(account_type):
+def get_members(group_list):
     """ 
     Returns a list of the usernames for all users with the
     given Linux group membership.
     """
     username=[]
-    for groupname in account_type
-        group=grp.getgrnam(groupname)
-        username.extend(group.gr_mem)
+    for groupname in group_list:
+        try: 
+            group=grp.getgrnam(groupname)
+            username.extend(group.gr_mem)
+        except:
+            pass
+ 
     return username 
 
-################
+def get_account_types(username):
+    """
+    Returns a list of current account types associated with the specified user.
+    """
+    accounts=[]
+    # Define premium account types (based on Linux groups)
+    # Priority accounts
+    priority=['priority','priority1','priority2','priority3','priority4',
+             'priority5','priority6','priority7','priority8','priority9']
+    priorityp=['priority+','priority+1']
+    # Premium GPU accounts
+    prigpu=['pri-gpu','pri-gpu1']
+    prigpup=['pri-gpu+','pri-gpu+1']
+    gpuhe=['gpu-he','gpu-he1']
+    # Bigmem accounts
+    pribigmem=['pri-bigmem','pri-bigmem1']
+ 
+    # Get list of all groups to which user belongs
+    proc=subprocess.Popen(['id','-Gn',username],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT,
+                           encoding='utf-8')
+    out,err=proc.communicate()
+    groups=list(out.strip('\n').split(" ")) 
+    
+    # Determine if user belongs to any groups associated with premium accounts 
+    for group in priority:
+      if group in groups:
+        accounts.append('priority')
+    for group in priorityp:
+      if group in groups:
+        accounts.append('priority+')
+    for group in prigpu:
+      if group in groups:
+        accounts.append('pri-gpu')
+    for group in prigpup:
+      if group in groups:
+        accounts.append('pri-gpu+')
+    for group in gpuhe:
+      if group in groups:
+        accounts.append('gpu-he')
+
+    # Add indicator to handle users with no premium account(s)
+    if not accounts:
+      accounts.append('-')
+
+    return accounts
+
+def get_user_name(username):
+    """
+    Returns user's name
+    """
+    tmp=[]
+
+    try:
+      tmp=pwd.getpwnam(username)
+      try:
+        output=tmp.pw_gecos.split(',')[0]
+      except:
+        output='NA'
+    except:
+      output='NA'
+
+    return output
+
+def get_user_email(username):
+    """ 
+    Returns user's email address
+    """
+    tmp=[]
+
+    try:
+      tmp=pwd.getpwnam(username)
+      try:
+        output=tmp.pw_gecos.split(',')[4]
+      except:
+        output='NA'
+    except:
+      output='NA'
+
+    return output
+
+def get_group(username):
+    """ 
+    Returns user's name
+    """
+    tmp=[]
+
+    try:
+      tmp=pwd.getpwnam(username)
+      try:
+        gid=tmp.pw_gid
+        tmp=grp.getgrgid(gid)
+        output=tmp.gr_name
+      except:
+        output='NA'
+    except:
+      output='NA'
+
+    return output
+
+##############
 # MAIN PROGRAM
-################
+##############
+
 # declarations
-users_priority=[]
+username=[]
+name={}
+email={}
+accounts={}
+primary={}
 
 # constants
 # oscar group names for premium account types
@@ -31,23 +141,27 @@ priorityp=['priority+','priority+1']
 prigpu=['pri-gpu','pri-gpu1']
 prigpup=['pri-gpu+','pri-gpu+1']
 gpuhe=['gpu-he','gpu-he1']
-# bigmem accounts
-pribigmem=['pri-bigmem','pri-bigmem1']
 
-# determine users belonging to each premium account groups
-print(priority)
-#users_priority=get_members(priority)
+# determine users with premium accounts
+username.extend(get_members(priority))
+username.extend(get_members(priorityp))
+username.extend(get_members(prigpu))
+username.extend(get_members(prigpup))
+username.extend(get_members(gpuhe))
+username=list(set(username))                  # eliminate duplicates
+username.sort()                               # sort alphabetically
 
-# create single list of all users and account types
-
-
-# for each user with a premium account, determine
-# name
-
-# email address
-
-# primary group affiliation
-
+# for each user, determine name, email address, premium account type
+for user in username:
+    name[user]=get_user_name(user)
+    email[user]=get_user_email(user)
+    accounts[user]=get_account_types(user)
+    primary[user]=get_group(user)
+ 
 # write out dtaa as a csv file
-print(users_priority)
+print(username)
+print(name)
+print(email)
+print(accounts)
+print(primary)
 
