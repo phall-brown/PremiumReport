8edc1077d53b20f501e1c237f0b9f290f2fed6ec
Paul Hall <paul_hall@brown.edu>
2021-11-16 08:59:26 -0500

    Updated code to handle priority++ accounts

--- PremiumReport.py
+++ PremiumReport.py
@@ -119,6 +119,7 @@ email={}
 primary={}
 account_priority={}
 account_priorityp={}
+account_prioritypp={}
 account_prigpu={}
 account_prigpup={}
 account_prigpupp={}
@@ -132,6 +133,7 @@ account_pribigmem={}
 priority=['priority','priority1','priority2','priority3','priority4',
           'priority5','priority6','priority7','priority8','priority9']
 priorityp=['priority+','priority+1']
+prioritypp=['priority++','priority++1']
 # premium GPU accounts
 prigpu=['pri-gpu','pri-gpu1']
 prigpup=['pri-gpu+','pri-gpu+1']
@@ -144,6 +146,7 @@ pribigmem=['pri-bigmem','pri-bigmem1']
 # determine users with premium accounts
 username.extend(get_members(priority))
 username.extend(get_members(priorityp))
+username.extend(get_members(prioritypp))
 username.extend(get_members(prigpu))
 username.extend(get_members(prigpup))
 username.extend(get_members(prigpupp))
@@ -160,6 +163,7 @@ for user in username:
     primary[user]=get_group(user)
     account_priority[user]=get_premium(user,priority)
     account_priorityp[user]=get_premium(user,priorityp)
+    account_prioritypp[user]=get_premium(user,prioritypp)
     account_prigpu[user]=get_premium(user,prigpu)
     account_prigpup[user]=get_premium(user,prigpup)
     account_prigpupp[user]=get_premium(user,prigpupp)
@@ -173,6 +177,7 @@ email_df=pd.DataFrame.from_dict(email,orient='index',columns=['Email'])
 primary_df=pd.DataFrame.from_dict(primary,orient='index',columns=['Group'])
 account_priority_df=pd.DataFrame.from_dict(account_priority,orient='index',columns=['priority'])
 account_priorityp_df=pd.DataFrame.from_dict(account_priorityp,orient='index',columns=['priority+'])
+account_prioritypp_df=pd.DataFrame.from_dict(account_prioritypp,orient='index',columns=['priority+'])
 account_prigpu_df=pd.DataFrame.from_dict(account_prigpu,orient='index',columns=['pri-gpu'])
 account_prigpup_df=pd.DataFrame.from_dict(account_prigpup,orient='index',columns=['pri-gpu+'])
 account_prigpupp_df=pd.DataFrame.from_dict(account_prigpupp,orient='index',columns=['pri-gpu++'])
@@ -182,6 +187,7 @@ account_pribigmem_df=pd.DataFrame.from_dict(account_pribigmem,orient='index',col
 
 # combine dataframes into a single dataframe
 data=pd.concat([name_df,email_df,primary_df,account_priority_df,account_priorityp_df,
+               account_prioritypp_df,
                account_prigpu_df,account_prigpup_df,account_prigpupp_df,
                account_gpuhe_df,account_gpuhep_df,account_pribigmem_df],
                axis=1,ignore_index=False) 
